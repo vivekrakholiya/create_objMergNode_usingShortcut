edf6db8cbb9d2d340f996a1f262d6ca4b24d693a
Vivek Rakholiya <107015608+vivekrakholiya@users.noreply.github.com>
2024-08-05 21:31:41 +0530

    Update main.py
    added refr inpute path

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import hou
+import hou , re
 
 def mergeCopiedNodes():
     network = hou.ui.curDesktop().paneTabUnderCursor()
@@ -8,6 +8,7 @@ def mergeCopiedNodes():
     copiedNodes = hou.ui.getTextFromClipboard()
 
     newPos = 0
+    a = 0
 
     if copiedNodes:
         list = copiedNodes.split()
@@ -16,13 +17,30 @@ def mergeCopiedNodes():
                 merge = hou.node(networkPath).createNode('object_merge','IN_'+item.split('/')[-1])
                 merge.parm('objpath1').set(str(item))
                 merge.parm('xformtype').set(1)
-                merge.setPosition(pos)
+                merge.setPosition(pos)             
                 merge.move([newPos*2,0])
                 if newPos == 0:
                     merge.setSelected(True,True)
                 else:
                     merge.setSelected(True,False)
+
                 newPos = newPos + 1
+
+                # create parameter in refr node
+                node = hou.node(item)
+                parm_template_group = node.parmTemplateGroup()
+            
+                for parm in node.parms():
+                    if 'op_path' in str(parm.name()):
+                        a = str(parm.name()).replace('op_path','')
+                    parm_label = "obj_mrg_path_" + str(int(a)+1)
+                    par_name = 'op_path' + str(int(a)+1)
+                op_path_parm = hou.StringParmTemplate(par_name,parm_label , 1, string_type=hou.stringParmType.NodeReference)
+                parm_template_group.addParmTemplate(op_path_parm)
+                node.setParmTemplateGroup(parm_template_group)
+                node.parm(par_name).set(merge.path())
+                        
+
 
 
 mergeCopiedNodes()
